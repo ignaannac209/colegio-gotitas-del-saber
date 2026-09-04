866bc3d2191699b8cb33fc59e7390f54f4bc0e2a
Estebangz01 <egodinez-2026507@kinal.edu.gt>
2026-09-04 13:44:55 -0600

    feature: implementacion de la clase rol;

---
+++ src/main/java/edu/ingsoft/colegio/gotitas/model/roles/rol.java
@@ -0,0 +1,20 @@
+
+package main.java.edu.ingsoft.colegio.gotitas.model.roles;
+
+
+public class rol {
+
+    private String nombreRol;
+
+    public rol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+
+    public String getNombreRol() {
+        return nombreRol;
+    }
+
+    public void setNombreRol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    } 
+}
