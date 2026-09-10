9ca8aa3c5eebf9d4a4e78bb8588eb4e2ea91ebb6
Jose Baten <josea.batenl@gmail.com>
2026-09-09 18:05:36 -0600

    feat: agrego controlador, vista fxml y css del dashboard de profesores

---
+++ src/edu/ingsoft/colegio/gotitas/controller/DashboardProfesoresController.java
@@ -0,0 +1,44 @@
+package edu.ingsoft.colegio.gotitas.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class DashboardProfesoresController implements Initializable {
+
+    @FXML
+    private Button btnCursos;
+
+    @FXML
+    private Button btnAsistencia;
+
+    @FXML
+    private Button btnNotas;
+
+    @FXML
+    private Button btnCerrarSesion;
+
+    @FXML
+    private TableView<?> tableAlumnos;
+
+    @FXML
+    private TableColumn<?, ?> colCarnet;
+
+    @FXML
+    private TableColumn<?, ?> colNombre;
+
+    @FXML
+    private TableColumn<?, ?> colGrado;
+
+    @FXML
+    private TableColumn<?, ?> colAccion;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Aquí puedes inicializar la lógica o cargar los datos de la base de datos al abrir la ventana.
+    }
+}
